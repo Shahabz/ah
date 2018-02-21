d7712d9c2305402b8d7373bffa3a791f3df3fcf5
paleonluna <paleonluna@gmail.com>
2018-02-20 23:59:11 -0800

    added in car driving scripts

---
+++ AmbroseHunter/Assets/Scripts/Vehicle/CarMetrics.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMetrics : MonoBehaviour {
+
+	public float currentGas = 50f;
+	public float maxGas = 50f;
+	public float currentHealth = 50f;
+	public float maxHealth = 100f;
+	public float currentReputation = 50f;
+	public float maxReputation = 50f;
+	public float currentStamina = 0f;
+	public float maxStamina = 100f;
+	float fakeGravityForce;
+
+	public Light[] headLights;
+	public Light[] tailLights;
+
+
+	[SerializeField] protected Material blackened;
+	[SerializeField] protected Transform[] m_hoverPoints;
+
+	protected Rigidbody m_body;
+	protected bool isTouchingGround;
+
+	protected float deadZone = 0.1f;
+	protected float hoverForce = 3000.0f;
+	protected float hoverHeight = 1.0f;
+
+	protected int m_layerMask;
+
+
+	public void TakeDamage(float x) {
+		currentHealth -= x;
+		if (currentHealth <= 0) {
+			Die ();
+		}
+	}
+
+	public virtual void Die () {
+
+	}
+
+	protected void Start() {
+		m_body = GetComponent<Rigidbody> ();
+		m_layerMask = 1 << LayerMask.NameToLayer ("Characters");
+		m_layerMask = ~m_layerMask;
+	}
+
+	public void FaceTarget(Vector3 target) {
+		transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, Quaternion.LookRotation(Vector3.RotateTowards (transform.forward, target - transform.position, .015f,.4f),Vector3.up).eulerAngles.y,transform.rotation.eulerAngles.z));
+	}
+
+	protected void BlackenCarMaterials() {
+		foreach (MeshRenderer x in GetComponentsInChildren<MeshRenderer>()) {
+			Material[] mats = new Material[x.materials.Length];
+			for (int i = 0; i < x.materials.Length; i++) { 
+				mats[i] = blackened;
+			}
+			x.materials = mats;
+		}
+	}
+
+	protected void ToggleLights(bool isOn){
+		foreach (Light x in tailLights) {
+			x.enabled = isOn;
+		}
+		foreach (Light y in headLights) {
+			y.enabled = isOn;
+		}
+	}
+
+	protected void FixedUpdate () {
+		HoverPhysics ();
+	}
+
+	protected void StabilizeMidAir () {
+		if (Mathf.Abs (transform.rotation.eulerAngles.x) > 1f) {
+			float negOrPosVal = (transform.rotation.eulerAngles.x < 180) || (transform.rotation.eulerAngles.x < 0) ? -1f : 1f;
+			transform.Rotate (negOrPosVal * 0.1f, 0, 0);
+		}
+		if (Mathf.Abs (transform.rotation.eulerAngles.z) > 1f) {
+			float negOrPosVal = (transform.rotation.eulerAngles.z < 180) ? -1f : 1f;
+			transform.Rotate (0, 0, negOrPosVal * 0.1f);
+		}
+	}
+
+	void HoverPhysics() {
+		RaycastHit hit;
+		for (int i = 0; i < m_hoverPoints.Length; i++) {
+			Transform hoverPoint = m_hoverPoints [i];
+			if (Physics.Raycast (hoverPoint.transform.position, -Vector3.up, out hit, hoverHeight, m_layerMask)) {
+				m_body.AddForceAtPosition (Vector3.up * hoverForce * (1.0f - (hit.distance / hoverHeight)), hoverPoint.transform.position);
+				isTouchingGround = true;
+			} else {
+				isTouchingGround = false;
+				StabilizeMidAir ();
+				if (transform.position.y > hoverPoint.transform.position.y) {
+					m_body.AddForceAtPosition (hoverPoint.transform.up * hoverForce, hoverPoint.transform.position);
+				} else {
+					m_body.AddForceAtPosition (hoverPoint.transform.up * -hoverForce, hoverPoint.transform.position);
+				}
+			}
+		}
+	}
+}
---
+++ AmbroseHunter/Assets/Scripts/Vehicle/PlayerCar.cs
@@ -0,0 +1,293 @@
+﻿   using UnityEngine;
+using System.Collections;
+using System;
+using InControl;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody), typeof(CarEngineSounds))]
+public class PlayerCar : CarMetrics
+{
+	#region variables
+	[SerializeField]
+	bool debugCarPosition, AIdebug;
+	[SerializeField]
+	Transform debugTransform;
+	[SerializeField]
+	GameObject debugAICam;
+	public static PlayerCar s_instance;
+
+	float aclAxis = 0;
+	public float forwardAcl = 30000.0f;
+	public float backwardAcl = 15000.0f;
+	public float strafeAcl = 5000.0f;
+	public float turnStrength = 5000f;
+	public float money;
+	float thrustForce = 300000f;
+	float currThrust = 0.0f;
+	float thrustCoolDown = 1f;
+	float currTurn = 0.0f;
+	float currStrafe;
+	public float idleGasLossRate, movingGasLossRate, currentGasLossRate;
+	public bool isAccelerating = false;
+	bool isMovementDisabled = false;
+	bool isTargeting;
+	public Transform currentTarget;
+
+	public Transform gameoverCam;
+	bool lerpGameOverSwitch;
+	float gameOverCamLerpTimer;
+	float gameOverCamLerpDuration = 10f;
+	Vector3 lerpStartPosition;
+	public Transform bulldozeChildTransform;
+	public Transform cameraEngagePosition;
+
+	public delegate void Target();
+	public static event Target BeginTarget;
+	
+	public delegate void Untarget();
+	public static event Untarget EndTarget;
+
+	public delegate void InteractEvent();
+	public static event InteractEvent PlayerInteractStart;
+
+	public delegate void DeinteractEvent();
+	public static event DeinteractEvent PlayerInteractEnd;
+
+	// mechanics
+	float staminaLossRate = .001f;
+
+	public enum MovementState {Drive, Target, Submit, OutOfGas};
+	public MovementState currMovementState = MovementState.Drive;
+	public bool isCharged, isBoosting;
+
+	InputDevice inputDevice;
+
+
+	#endregion
+	void Awake() {
+
+		if (s_instance == null) {
+			s_instance = this;
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
+
+	new void Start ()
+	{
+		base.Start ();
+		if (AIdebug) {
+			Camera.main.gameObject.SetActive (false);
+			debugAICam.SetActive (true);
+		}
+		else if (debugCarPosition) {
+			transform.position = debugTransform.position;
+		}
+
+	}
+	
+	void Update ()
+	{
+		if (!AIdebug) {
+			//State Machine
+			switch (currMovementState) {
+				case MovementState.OutOfGas :
+					LoseGame ();
+				break;
+			}
+
+			inputDevice = InputManager.ActiveDevice;
+
+			if (currentGas < 0.5f) {
+				RunOutOfGas ();
+			}
+			HandleGasLoss ();
+
+			#region Input Handling
+			//Car Movement
+			HandleLeftStickVertical ();
+			HandleLeftStickHorizontal ();
+			HandleRightStickHorizontal ();
+
+			//Interaction Functions
+			if (inputDevice.Action1.WasPressed) {
+				PlayerInteractStart ();
+			}
+
+			if (inputDevice.Action1.WasReleased) {
+				PlayerInteractEnd ();
+			}
+
+			if (inputDevice.LeftTrigger.WasPressed) {
+				LockOnToTarget ();
+			}
+			if (inputDevice.LeftTrigger.WasReleased) {
+				UnlockFromTarget ();
+			}
+
+			if (inputDevice.RightTrigger.WasPressed) {
+				AfterburnerBoost ();
+			}
+			#endregion
+		}
+
+	}
+
+	#region Input Functions
+	void UnlockFromTarget() {
+		currMovementState = MovementState.Drive;
+		EndTarget ();
+		isTargeting = false;
+	//	Camera.main.GetComponent<HoverFollowCam> ().thisCameraMode = HoverFollowCam.CameraMode.normalMode;
+	}
+
+	void LockOnToTarget () {
+		currMovementState = MovementState.Target;
+		BeginTarget ();
+		isTargeting = true;
+	//	Camera.main.GetComponent<HoverFollowCam> ().thisCameraMode = HoverFollowCam.CameraMode.targetMode;
+	}
+
+	void AfterburnerBoost() {
+		if (!isBoosting) {
+			isBoosting = true;
+			StartCoroutine ("IsBoosting");
+		}
+	}
+
+	void HandleLeftStickVertical () {
+		//Car Acceleration
+		currThrust = 0.0f;
+		isAccelerating = false;
+		aclAxis = 0;
+		if (inputDevice.LeftStickUp != 0) {
+			aclAxis = inputDevice.LeftStickUp;
+			isAccelerating = true;
+		} else if (inputDevice.LeftStickDown != 0) {
+			aclAxis = -inputDevice.LeftStickDown;
+			isAccelerating = true;
+		}
+		if (aclAxis > deadZone)
+			currThrust = aclAxis * forwardAcl;
+		else if (aclAxis < -deadZone)
+			currThrust = aclAxis * backwardAcl;
+	}
+
+	void HandleRightStickHorizontal () {
+		// Turning
+		float turnAxis = 0.0f;
+		if (inputDevice.RightStickRight != 0) {
+			turnAxis = inputDevice.RightStickRight;
+		} else if (inputDevice.RightStickLeft != 0) {
+			turnAxis = -inputDevice.RightStickLeft;
+		}
+		currTurn = turnAxis;
+	}
+
+	void HandleLeftStickHorizontal () {
+		// Strafe
+		float strafeAxis = 0;
+		if (inputDevice.LeftStickLeft != 0) {
+			strafeAxis = -inputDevice.LeftStickLeft;
+		} else if (inputDevice.LeftStickRight != 0) {
+			strafeAxis = inputDevice.LeftStickRight;
+		}
+		currStrafe = strafeAxis * strafeAcl;
+	}
+	#endregion
+
+	void StartGameOverCameraLerp () {
+		lerpStartPosition = Camera.main.transform.position;
+		gameOverCamLerpTimer = Time.time;
+		lerpGameOverSwitch = true;
+	}
+	new void FixedUpdate ()
+	{
+		base.FixedUpdate ();
+		// Turn
+		if (currTurn != 0 && !isMovementDisabled) {
+			m_body.AddRelativeTorque (Vector3.up * currTurn * turnStrength);
+		}
+
+
+		//if (currStrafe != 0 && !isMovementDisabled) {
+	//		m_body.AddForce (transform.right * currStrafe);
+	//	}
+
+		//if (isTargeting && currentTarget!=null) {
+		//	FaceTarget (currentTarget.position);
+		//}
+
+		if (currMovementState == MovementState.Drive) {
+			// Forward
+			if (Mathf.Abs (currThrust) > 0 && isTouchingGround) {
+				m_body.AddForce (transform.forward * currThrust);
+			} else {
+				m_body.AddForce (transform.forward * currThrust);
+				//correct rotation
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "NPC") {
+		//	other.GetComponent<AIHoverCar> ().isCloseToPlayer = true;
+		//	other.GetComponent<AIHoverCar> ().currentTarget = transform;
+			currentTarget = other.transform;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.tag == "NPC") {
+		//	other.GetComponent<AIHoverCar> ().isCloseToPlayer = false;
+		//	other.GetComponent<AIHoverCar> ().currentTarget = null;
+			currentTarget = null;
+		}
+	}
+
+	void HandleGasLoss () {
+		if (isAccelerating) {
+			currentGasLossRate = movingGasLossRate;
+		} else {
+			currentGasLossRate = idleGasLossRate;
+		}
+		currentGas -= currentGasLossRate;
+	}
+
+	IEnumerator IsBoosting () {
+		GetComponent<CarEngineSounds> ().PlayBoostSound ();
+		m_body.AddForce (transform.forward * thrustForce);
+		//GUIManager.s_instance.thrustSlider.value = thrustCoolDown;
+		yield return new WaitForSeconds(thrustCoolDown);
+		isBoosting = false;
+	}
+
+	void RunOutOfGas () {
+		currMovementState = MovementState.OutOfGas;
+		//Camera.main.GetComponent<HoverFollowCam> ().enabled = false;
+	}
+
+	void LoseGame() {
+		isMovementDisabled = true;
+		if (!lerpGameOverSwitch) {
+			StartGameOverCameraLerp ();
+		}
+		float timeElapsed = Time.time - gameOverCamLerpTimer;
+		float percentage = timeElapsed / gameOverCamLerpDuration;
+		Camera.main.transform.position = Vector3.Lerp (lerpStartPosition, gameoverCam.position, percentage);
+		Camera.main.transform.LookAt (transform.position);
+	}
+
+	void StealGas() {
+		
+	}
+
+	void OnCollisionEnter (Collision thisCollision) {
+//		if (thisCollision.collider.gameObject.tag == "NPC" && currMovementState == MovementState.Target) {
+//			StealGas();
+//		}
+	}
+
+}
+
